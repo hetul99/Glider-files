ae563aeff959f7d42e3d50ebfa2e44eceb367357
hetul99 <46005122+hetul99@users.noreply.github.com>
2024-05-22 08:29:23 -0400

    Update search_replace.sh

--- bash-search-replace/search_replace.sh
+++ bash-search-replace/search_replace.sh
@@ -2,6 +2,7 @@
 
 # Check if the correct number of arguments are provided
 if [ "$#" -ne 1 ]; then
+  echo "Error: You must provide exactly one input file as an argument."
   echo "Usage: $0 input_file"
   exit 1
 fi
@@ -13,3 +14,6 @@ output_file="${input_file}.new"
 sed 's/SEARCH/REPLACE/g' "$input_file" > "$output_file"
 
 echo "All occurrences of 'SEARCH' have been replaced with 'REPLACE' in $output_file"
+
+
+# https://stackoverflow.com/questions/8002981/using-sed-to-replace-a-string-with-another-string
