f2f600d78b3d78d51d98437c5ed85f04b5781026
hetul99 <46005122+hetul99@users.noreply.github.com>
2024-05-22 08:58:55 -0400

    Update and rename exchange-btw-ec2-terraform-sns-sqs/ec2-B.tf to exchange-btw-ec2-terraform-sns-sqs/account_b/ec2.tf

--- exchange-btw-ec2-terraform-sns-sqs/account_b/ec2.tf
+++ exchange-btw-ec2-terraform-sns-sqs/account_b/ec2.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "server_b" {
   provider       = aws.account_b
   ami            = "ami-0c55b159cbfafe1f0"
   instance_type  = "t2.micro"
-  user_data      = file("server_b_userdata.sh")
+  user_data      = file("${path.module}/userdata/server_b_userdata.sh")
   tags = {
     Name = "server-b"
   }
