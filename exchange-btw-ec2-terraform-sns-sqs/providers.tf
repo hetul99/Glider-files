9db49af26b0da9f1d59f00614c3b0d892f09b885
hetul99 <46005122+hetul99@users.noreply.github.com>
2024-05-22 08:44:24 -0400

    Update providers.tf

--- exchange-btw-ec2-terraform-sns-sqs/providers.tf
+++ exchange-btw-ec2-terraform-sns-sqs/providers.tf
@@ -1,6 +1,18 @@
 provider "aws" {
   alias  = "account_a"
   region = "us-east-1"
+  profile = var.profile_account_a
+}
+
+provider "aws" {
+  alias  = "account_b"
+  region = "us-east-1"
+  profile = var.profile_account_b
+}
+
+variable "aws_account_a" {
+  description = "AWS Account A ID"
+  type        = string
 }
 
 variable "aws_account_b" {
@@ -8,12 +20,14 @@ variable "aws_account_b" {
   type        = string
 }
 
-provider "aws" {
-  alias  = "account_b"
-  region = "us-east-1"
+variable "profile_account_a" {
+  description = "AWS CLI profile for Account A"
+  type        = string
+  default     = "default"
 }
 
-variable "aws_account_a" {
-  description = "AWS Account A ID"
+variable "profile_account_b" {
+  description = "AWS CLI profile for Account B"
   type        = string
+  default     = "default"
 }
